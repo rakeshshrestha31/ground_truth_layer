c6d7d92c3344064d3b73167f911c031d19bfb24e
rakeshs <rakesh_shrestha@sfu.ca>
2018-06-14 14:01:20 -0700

    first commit: basic setup for pluginlib

---
+++ include/ground_truth_layer/ground_truth_layer.h
@@ -0,0 +1,53 @@
+//
+// Created by rakesh on 14/06/18.
+//
+
+#ifndef GROUND_TRUTH_LAYER_GROUND_TRUTH_LAYER_H
+#define GROUND_TRUTH_LAYER_GROUND_TRUTH_LAYER_H
+
+#include <ros/ros.h>
+#include <costmap_2d/costmap_layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+namespace ground_truth_layer
+{
+class GroundTruthLayer : public costmap_2d::Layer
+{
+public:
+  GroundTruthLayer();
+  virtual ~GroundTruthLayer();
+
+  virtual void onInitialize();
+  virtual void activate();
+  virtual void deactivate();
+  virtual void reset();
+
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y,
+                            double* max_x, double* max_y);
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+
+  virtual void matchSize();
+
+  void callback(const sensor_msgs::LaserScanConstPtr &laser_scan,
+                const nav_msgs::OdometryConstPtr &odometry);
+
+protected:
+  ros::NodeHandlePtr local_nh_;
+  ros::NodeHandlePtr global_nh_;
+
+  boost::shared_ptr< message_filters::Subscriber<sensor_msgs::LaserScan> > laser_sub_;
+  boost::shared_ptr< message_filters::Subscriber<nav_msgs::Odometry> > odometry_sub_;
+  boost::shared_ptr< message_filters::TimeSynchronizer<sensor_msgs::LaserScan, nav_msgs::Odometry> > topic_sync_;
+
+  std::string odometry_topic_;
+  std::string laser_topic_;
+
+};
+} // namespace ground_truth_layer
+
+#endif //GROUND_TRUTH_LAYER_GROUND_TRUTH_LAYER_H
---
+++ plugins/ground_truth_layer.cpp
@@ -0,0 +1,51 @@
+//
+// Created by rakesh on 14/06/18.
+//
+
+#include <ground_truth_layer/ground_truth_layer.h>
+#include <costmap_2d/costmap_math.h>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(ground_truth_layer::GroundTruthLayer, costmap_2d::Layer)
+
+namespace ground_truth_layer
+{
+
+GroundTruthLayer::GroundTruthLayer()
+{
+  // TODO
+}
+
+GroundTruthLayer::~GroundTruthLayer()
+{
+  // TODO
+}
+
+void GroundTruthLayer::onInitialize()
+{
+  local_nh_.reset(new ros::NodeHandle("~/" + name_));
+  global_nh_.reset(new ros::NodeHandle());
+
+  local_nh_->param("odom_topic",  odometry_topic_, std::string("odom"));
+  local_nh_->param("laser_topic", laser_topic_,    std::string("scan"));
+
+  laser_sub_.reset(new message_filters::Subscriber<sensor_msgs::LaserScan>(*global_nh_, laser_topic_, 1));
+  odometry_sub_.reset(new message_filters::Subscriber<nav_msgs::Odometry>(*global_nh_, odometry_topic_, 1));
+  topic_sync_.reset(
+    new message_filters::TimeSynchronizer<sensor_msgs::LaserScan, nav_msgs::Odometry>(
+      *laser_sub_,
+      *odometry_sub_,
+      2
+    )
+  );
+  topic_sync_->registerCallback(boost::bind(&GroundTruthLayer::callback, this, _1, _2));
+}
+
+void GroundTruthLayer::callback(const sensor_msgs::LaserScanConstPtr &laser_scan,
+                                const nav_msgs::OdometryConstPtr &odometry)
+{
+  ROS_INFO("Received!!!");
+}
+
+} // namespace ground_truth_layer
+
